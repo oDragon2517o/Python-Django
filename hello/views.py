c9d3fa986b5967d2ad8788679148729bf4dbc452
Dragon2517 <rabvasya1994@yandex.ru>
2023-02-22 15:42:21 +0300

    2 глава разобрана

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
   
 def Hello(request):
     return HttpResponse("Hello METANIT.COM")
 
-def index(request):
+#def index(request):
 
-    host = request.META["HTTP_HOST"] # получаем адрес сервера
-    user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные бразера
-    path = request.path     # получаем запрошенный путь
+#    host = request.META["HTTP_HOST"] # получаем адрес сервера
+#    user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные бразера
+#    path = request.path     # получаем запрошенный путь
      
-    return HttpResponse(f"""
-        <p>Host: {host}</p>
-        <p>Path: {path}</p>
-        <p>User-agent: {user_agent}</p>
-    """
-        "Hello METANIT.COM", headers={"SecretCode": "21234567"})
+#    return HttpResponse(f"""
+#        <p>Host: {host}</p>
+#        <p>Path: {path}</p>
+#        <p>User-agent: {user_agent}</p>
+#    """
+#        "Hello METANIT.COM", headers={"SecretCode": "21234567"})
 
 def txt(request):
     return HttpResponse("<h1>Hello</h1>", content_type="text/plain", charset="utf-8")
@@ -42,3 +43,63 @@ def user0(request):
   
 def user(request, name="Undefined", age =0):
     return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+
+#####################
+ 
+def index1(request):
+    return HttpResponse("Главная страница")
+ 
+def products(request, id):
+    return HttpResponse(f"Товар {id}")
+ 
+def comments(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
+ 
+def questions(request, id):
+    return HttpResponse(f"Вопросы о товаре {id}")
+
+####################
+
+def index3(request):
+    return HttpResponse("<h2>Главная</h2>")
+  
+def user3(request):
+    age = request.GET.get("age",0)
+    name = request.GET.get("name", "Undefined")
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+##############################
+
+def index4(request):
+    return HttpResponse("Index")
+ 
+def about(request):
+    return HttpResponse("About")
+ 
+def contact(request):
+    return HttpResponseRedirect("/about")
+ 
+def details(request):
+    return HttpResponsePermanentRedirect("/")
+
+##################
+#def index(request):
+#    return JsonResponse({"name": "Tom", "age": 38})
+
+def index(request):
+    bob = Person("Bob", 41)
+    return JsonResponse(bob, safe=False, encoder=PersonEncoder)
+ 
+class Person:
+  
+    def __init__(self, name, age):
+        self.name = name    # имя человека
+        self.age = age        # возраст человека
+ 
+class PersonEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Person):
+            return {"name": obj.name, "age": obj.age}
+            # return obj.__dict__
+        return super().default(obj)
--- metanit/urls.py
+++ metanit/urls.py
@@ -13,22 +13,49 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from hello import views
  
+
+product_patterns = [
+    path("", views.products),
+    path("comments", views.comments),
+    path("questions", views.questions),
+]
+ 
+#urlpatterns = [
+#    path("", views.index1),
+#    path("products/", include(product_patterns)),
+#]
+
+
 urlpatterns = [
-    path('', views.index),
+    #path('', views.index),
+    path("", views.index),
+    path("products/<int:id>/", include(product_patterns)),
+
+
     path("index", views.index),
     path("error", views.error),
     path("txt", views.txt),
-    path('about', views.about, kwargs={"name":"Tom", "age": 38}),
-    re_path(r'^contact', views.contact),
+    #path('about', views.about, kwargs={"name":"Tom", "age": 38}),
+    #re_path(r'^contact', views.contact),
     #path("user", views.user),
     #path("user/<name>", views.user),
     #path("user/<name>/<int:age>", views.user),
     re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
     re_path(r"^user/(?P<name>\D+)", views.user),
-    re_path(r"^user", views.user),
+    #re_path(r"^user", views.user),
+
+    ###########
+    path("", views.index),
+    path("about/", views.about),
+    path("contact/", views.contact),
+    path("details/", views.details),
+
+
+
+    path("user/", views.user3),
 
     path('Hello', views.Hello),
 ]
